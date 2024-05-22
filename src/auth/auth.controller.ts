315b52493f4c2e6b2a78790d5ffc5b3d98263c1d
unknown <sordyq4242@gmail.com>
2024-05-22 17:19:06 +0100

    serialize interceptor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dtos/register.dto';
+import { Serialize } from 'src/interceptors/serialize-interceptor';
+import { AuthDto } from './dtos/auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+@Serialize(AuthDto)
+export class AuthController {
+    constructor( private readonly authService: AuthService){}
+
+    @Post('register')
+    async register(@Body() registerUserDto: RegisterUserDto){
+        return this.authService.register(registerUserDto)
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,20 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterUserDto } from './dtos/register.dto';
+import * as bcrypt from 'bcryptjs'
 
 @Injectable()
 export class AuthService {
     constructor(private usersService: UsersService){}
 
     async register(registerUserDto: RegisterUserDto){
-        const user = await this.usersService.find(registerUserDto.email);
-        if(user.length){
+        const users = await this.usersService.find(registerUserDto.email);
+        if(users.length){
             throw new BadRequestException("User Already Exist")
         }
+        const hashPassword = await bcrypt.hash(registerUserDto.password, 10);
+        const user = await this.usersService.create({...registerUserDto, password: hashPassword});
+
+        return user;
     }
 }
